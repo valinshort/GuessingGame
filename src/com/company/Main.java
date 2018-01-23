d4aac24b62cc045ba910ca09b64ff5c3827db859
Valin Short <valin.short1998@gmail.com>
2018-01-23 16:20:53 -0500

    Allowed and prompted for user input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
+        // generate a random number and allow the user to put in a number in
+        // we will then tell the user if the number is too high or too low
+        // allow for five chances, if the number isnt guessed after these tries then tell the user what the number is and ask the user if they want to play again
+        // if the get it right, congradulate them, and then ask them if they want to play again
+
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your guess;");
+        int userGuess = input.nextInt();
+
+
 
 
 
