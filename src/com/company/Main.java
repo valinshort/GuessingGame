4a6b5884d747df68cfe0e235f84d944345e115b9
Valin Short <valin.short1998@gmail.com>
2018-01-26 15:30:58 -0500

    guessing game

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         // generate a random number and allow the user to put in a number in
         // we will then tell the user if the number is too high or too low
@@ -12,11 +12,43 @@ public class Main {
         // if the get it right, congradulate them, and then ask them if they want to play again
 
 
-
         Scanner input = new Scanner(System.in);
+        int numberOfGuess = 0;
+        final int Max_GUESS_Count = 4;
         System.out.println("Enter your guess;");
+
+
         int userGuess = input.nextInt();
 
+
+        int generateNumber = (int) Math.ceil(Math.random() * 100);
+
+        while (numberOfGuess < +5)
+            ;
+        if (userGuess > generateNumber) {
+            System.out.println("your number is too high");
+            numberOfGuess++;
+        } else if (userGuess < generateNumber) {
+            System.out.println("your number is too low");
+            numberOfGuess++;
+
+        else if (userGuess == generateNumber) {
+                System.out.println("you guessed the correct number");
+            } else if (numberOfGuess == Max_GUESS_Count) ;
+
+
+        } while {
+            numberOfGuess <= 4;
+        }
+
+
+     if(numberOfGuess ==5)
+
+    {
+        System.out.println("you have run out of guesses");
+
+
+    }
 
 
 
